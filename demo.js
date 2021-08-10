c50c6c18af4b000cf5cd8ce05b4fd7f77cc17963
Bobby Allen <84739778+bamagrad028@users.noreply.github.com>
2021-08-10 10:01:32 -0500

    Game updated

---
+++ demo.js
@@ -0,0 +1,23 @@
+// We need a way to have a random index item in a array
+//Math.random()
+
+// I want an array of domino list items, and I will pick a random one using its index 
+//between 0 and lenght
+
+//Nodelist -> Array.from() - Array
+
+let dominoElements = document.querySelectorAll("#dominoes-list li")
+console.log(dominoElements)
+
+function getRandomNumber (min, max) {
+    // Source: https://stackoverflow.com/a/7228322/1034856
+    // License: https://creativecommons.org/licenses/by-sa/4.0/
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+let firstIndex = 0
+let lastIndex = dominoElements.length - 1
+let randomIndex = getRandomNumber(firstIndex,lastIndex)
+
+let randomDomino = dominoElements[randomIndex]
+randomDomino.classList.add("selected")
